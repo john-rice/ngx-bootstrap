8d450d0fd3d88cd62e3f52c4c238388eedfe17d7
Alexey Umanskiy <sharkakatsukie@gmail.com>
2025-01-02 12:26:21 +0200

    refactor(docs): changed to non hashbased routing and ssr (#6711)
    * refactor(docs): changed to non hashbased routing and ssr
    
    * refactor(docs): updated package-lock.json
    
    * fix(e2e): updated e2e
    
    * feat(docs): updated ci
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development
    
    * feat(ssr-docs); merged development

--- apps/ngx-bootstrap-docs/src/app/app.module.ts
+++ apps/ngx-bootstrap-docs/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation  } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { environment } from '../environments/environment';
 import { ngdoc } from '../ng-api-doc';
 import { AppComponent } from './app.component';
 import { DOCS_TOKENS, DocsModule, NgApiDoc, SIDEBAR_ROUTES, SidebarRoutesStructure } from '@ngx-bootstrap-doc/docs';
@@ -14,8 +13,8 @@ import { routes } from './app.routing';
     ],
     bootstrap: [AppComponent], imports: [BrowserAnimationsModule,
         DocsModule,
-        RouterModule.forRoot(routes, { useHash: environment.useHash })
         ], providers: [
+        provideRouter(routes, withEnabledBlockingInitialNavigation()),
         { provide: NgApiDoc, useValue: ngdoc },
         { provide: DOCS_TOKENS, useValue: routes },
         { provide: SIDEBAR_ROUTES, useValue: SidebarRoutesStructure },
